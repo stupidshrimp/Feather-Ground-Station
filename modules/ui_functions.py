ae9be22b16fa7eebe966b5a2dea8450ea3424556
stupidshrimp <123285311+stupidshrimp@users.noreply.github.com>
2025-09-09 08:24:26 -0500

    Fix toggleLeftBox animation when disabled

--- modules/ui_functions.py
+++ modules/ui_functions.py
@@ -155,8 +155,8 @@ class UIFunctions:
                 widthExtended = standard
                 # RESET BTN
                 self.ui.toggleLeftBox.setStyleSheet(style.replace(color, ''))
-                
-        UIFunctions.start_box_animation(self, width, widthRightBox, "left")
+
+            UIFunctions.start_box_animation(self, width, widthRightBox, "left")
 
     # TOGGLE RIGHT BOX
     # ///////////////////////////////////////////////////////////////
